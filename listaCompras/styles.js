05b4b471c0ea8862d1bbbe04aa4ce8bc616e7b92
nicoleribeiro <nicole.ribeiro1612@gmail.com>
2022-10-07 20:50:50 -0300

    app lista de compras

---
+++ listaCompras/styles.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from "react-native"
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: 'center'
+  },
+  title: {
+    width: '100%',
+    textAlign: 'center',
+    marginTop: 10,
+    paddingBottom: 5,
+    fontSize: 22,
+    borderBottomWidth: 1,
+    borderBottomColor: '#333'
+  },
+  input: {
+    width: '60%',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCC',
+    marginBottom: 10,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  input_2: {
+    width: '40%',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCC',
+    marginBottom: 10,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  inputs: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  }
+})
+
+export { styles }
